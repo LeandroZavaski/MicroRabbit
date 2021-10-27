28d63949c153cba416061f3e911b300046bb76a0
Leandro Zavaski <leandro.zavaski@xpi.com.br>
2021-10-27 18:03:44 -0300

    first commit

---
+++ ProducerConsumerRabbitMQ/Producer/Sender.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                string message = "Getting start with .NET Core RabbitMQ!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "BasicTest", null, body);
+
+                Console.WriteLine($"Send the message: {message} ...");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
